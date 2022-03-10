b81414adf042c3f6d283884834cac8a75602135e
Eric Russell <esrussell.us@gmail.com>
2022-03-10 12:55:31 -0700

    new file

---
+++ exercises.rb
@@ -0,0 +1,8 @@
+movies = {
+    :"The Shawshank Redemption" => 1994,
+    :"The Godfather" => 1972,
+    :"The Dark Knight" => 2008,
+    :"The Godfather: Part II" => 1974,
+    :"12 Angry Men" => 1957
+  }
+puts movies
