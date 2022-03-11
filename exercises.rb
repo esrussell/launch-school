b9d2d0c705433a9a5b861fcdfd38607f309b29cb
Eric Russell <esrussell.us@gmail.com>
2022-03-11 08:02:05 -0700

    new exercise

--- exercises.rb
+++ exercises.rb
@@ -9,10 +9,23 @@
 # puts movies [:the_godfather_part_ii]
 # puts movies [:twelve_angry_men]
 
-movies = {  :the_shawshank_redemption => 1994,
-            :the_godfather => 1972,
-            :the_dark_knight => 2008,
-            :the_godfather_part_ii => 1974,
-            :twelve_angry_men => 1957}
-for i in movies.do
-  done
+# movies = {  :the_shawshank_redemption => 1994,
+#             :the_godfather => 1972,
+#             :the_dark_knight => 2008,
+#             :the_godfather_part_ii => 1974,
+#             :twelve_angry_men => 1957}
+# for i in movies.do
+#   done
+
+# movies = [1994, 1972, 2008, 1974, 1957]
+# puts movies [0]
+# puts movies [1]
+# puts movies [2]
+# puts movies [3]
+# puts movies [4]
+
+# movies = [1994, 1972, 2008, 1974, 1957]
+# movies.each do |item|
+#   puts item
+# end
+
