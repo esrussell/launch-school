ca7a59338afaf80d5730125ed86000154e7cdf5e
Eric Russell <esrussell.us@gmail.com>
2022-03-11 06:56:44 -0700

    Use an iterator instead of 'puts' to return a list of movie release dates

---
+++ exercises.rb
@@ -0,0 +1,19 @@
+# movies = {  :the_shawshank_redemption => 1994,
+#             :the_godfather => 1972,
+#             :the_dark_knight => 2008,
+#             :the_godfather_part_ii => 1974,
+#             :twelve_angry_men => 1957}
+# puts movies [:the_shawshank_redemption]
+# puts movies [:the_godfather]
+# puts movies [:the_dark_knight]
+# puts movies [:the_godfather_part_ii]
+# puts movies [:twelve_angry_men]
+
+movies = {  :the_shawshank_redemption => 1994,
+            :the_godfather => 1972,
+            :the_dark_knight => 2008,
+            :the_godfather_part_ii => 1974,
+            :twelve_angry_men => 1957}
+for i in movies.do
+  done
+
