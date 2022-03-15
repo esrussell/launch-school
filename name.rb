5d5905c98c17ede13adaa576a89bb5239b4b335d
Eric Russell <esrussell.us@gmail.com>
2022-03-15 12:39:13 -0600

    file cleanup

---
+++ name.rb
@@ -0,0 +1,15 @@
+# name.rb
+
+# puts 'What is your name?'
+# name = gets.chomp
+# puts 'Welcome to Variables, ' + name + '!'
+
+# 10.times do
+#   puts name
+# end
+
+puts 'What is your first name?'
+first_name = gets.chomp
+puts 'What is your last name?'
+last_name = gets.chomp
+puts "Your name is " + first_name + " " + last_name
